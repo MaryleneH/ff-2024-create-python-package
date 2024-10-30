282051688bce9428390d8ca5ad1a72b05a6de838
Marylène Henry <marylene.henry@gmail.com>
2024-10-30 16:15:24 +0000

    [ADD] add_numbers function

---
+++ src/mypackage_test/add_numbers.py
@@ -0,0 +1,22 @@
+"""A module that adds numbers together"""
+
+def add_numbers(num_1, num_2):
+    """Add number together
+
+
+    Parameters
+    ----------------
+
+    Args:
+        num_1 (int):
+            the first number to add
+        num_2 (int): 
+            the second number to add 
+
+    Return 
+    ----------------
+
+        the sum of two numbers 
+    """
+
+    return num_1 + num_2
